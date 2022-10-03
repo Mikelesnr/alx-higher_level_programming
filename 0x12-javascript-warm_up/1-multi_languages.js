fe46debab4d96b9e9f305f79e4df4011d45bd6ec
Mikelesnr <mwanza.n.m@gmail.com>
2022-10-03 13:58:07 +0200

    1-multi_languages.js

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,4 +1,5 @@
-#!/usr/bin/node
-console.log('C is fun');
-console.log('Python is cool');
-console.log('Javascript is amazing');
+#!/usr/bin/
+const firstLine = "C is fun";
+const secondLine = "Python is cool";
+const thirdLine = "Javascript is amazing";
+console.log(firstLine, secondLine, thirdLine);
