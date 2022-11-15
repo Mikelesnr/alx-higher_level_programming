fbd27cf2b774a50fc0517468ac588b3f43fae497
Mikelesnr <mwanza.n.m@gmail.com>
2022-11-16 00:30:42 +0200

    error fixes

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -1,23 +1,19 @@
 #!/usr/bin/node
+
 const request = require('request');
-const url = 'http://swapi.co/api/films/' + process.argv[2];
-let movieDict = {};
-let characterDict = {};
-let i = 0;
-request(url, function (error, response, body) {
-  if (error) {
-    console.log(error);
-  } else {
-    movieDict = JSON.parse(body);
-    for (i = 0; i < movieDict.characters.length; i++) {
-      request(movieDict.characters[i], function (error, request, body) {
-        if (error) {
-          console.log(error);
-        } else {
-          characterDict = JSON.parse(body);
-          console.log(characterDict.name);
-        }
-      });
-    }
+const filmId = process.argv[2];
+const url = `https://swapi-api.hbtn.io/api/films/${filmId}`;
+
+request(url, function (err, response, body) {
+  if (err) {
+    console.log(err);
+  }
+  for (const characterId of JSON.parse(body).characters) {
+    request(characterId, function (err, response, body) {
+      if (err) {
+        console.log(err);
+      }
+      console.log(JSON.parse(body).name);
+    });
   }
 });
--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,12 +1,12 @@
 #!/usr/bin/node
+
 const request = require('request');
-const url = 'http://swapi.co/api/films/' + process.argv[2];
-let resultDict = {};
-request(url, function (error, response, body) {
-  if (error) {
-    console.log(error);
-  } else {
-    resultDict = JSON.parse(body);
-    console.log(resultDict.title);
+const id = process.argv[2];
+const url = `https://swapi-api.hbtn.io/api/films/${id}`;
+
+request(url, function (err, response, body) {
+  if (err) {
+    console.log(err);
   }
+  console.log(JSON.parse(body).title);
 });
